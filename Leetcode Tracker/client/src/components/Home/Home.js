5d2ccb1a063360813f57d2d9c87d6be5854cb47e
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-12-29 01:03:30 +0530

    made basic frontend to start working with backend

---
+++ Leetcode Tracker/client/src/components/Home/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Container,Grid} from '@material-ui/core';
+import UsersList from './UsersList/UsersList';
+import WeeklyTop from './WeeklyTop/WeeklyTop'
+import User from './UsersList/User/User';
+function Home() {
+    return (
+     
+        <>
+            <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+
+                <Grid item xs={12} sm={8}>
+
+                    <UsersList/>
+
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+
+                    <WeeklyTop/>
+
+                </Grid>
+
+
+            </Grid>
+        
+        </>
+
+
+      
+    )
+}
+
+export default Home
