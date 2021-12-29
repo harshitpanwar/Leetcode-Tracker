5dd75d85e5dbddb5a80f76bcd105eb60bc6006e7
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-12-30 00:43:22 +0530

    index file for the client

--- Leetcode Tracker/client/src/index.js
+++ Leetcode Tracker/client/src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>
+   ,
   document.getElementById('root')
 );
