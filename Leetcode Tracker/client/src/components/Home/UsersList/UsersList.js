2d20f9e258d9d5e3e601b17254f9068c71a862ba
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-12-31 02:56:42 +0530

    added use selector functionality

--- Leetcode Tracker/client/src/components/Home/UsersList/UsersList.js
+++ Leetcode Tracker/client/src/components/Home/UsersList/UsersList.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import User from './User/User'
 import './UsersList.css'
+import { useSelector } from 'react-redux';
 
 const UsersList = () => {
+
+
+    const posts = useSelector((state) => state.posts);
+
+    console.log(posts);
+
+
     return (
         <div className='UsersList'>
             UsersLIs
