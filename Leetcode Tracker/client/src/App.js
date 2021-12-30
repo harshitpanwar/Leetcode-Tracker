c6c7057149ddef8a718aebd0331b3b349f8d6765
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-12-31 01:39:52 +0530

    added dispatch method in the App.js file

--- Leetcode Tracker/client/src/App.js
+++ Leetcode Tracker/client/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header/Header';
 import './App.css'
 import Home from './components/Home/Home';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 
-function App() {
+
+const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts);
+  }, [dispatch]);
+
   return (
     <div>
         <Header/>
         <Home/>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
