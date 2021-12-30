151705dc7e776047828c296139636b021f888e19
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-12-31 02:55:40 +0530

    added routes from react router

--- Leetcode Tracker/client/src/App.js
+++ Leetcode Tracker/client/src/App.js
@@ -2,8 +2,11 @@ import React, { useEffect } from 'react';
 import Header from './components/Header/Header';
 import './App.css'
 import Home from './components/Home/Home';
+import About from './components/About/About';
+import AddUser from './components/AddUser/AddUser';
 import { useDispatch } from 'react-redux';
 import { getPosts } from './actions/posts';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 const App = () => {
@@ -11,14 +14,30 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getPosts);
+    dispatch(getPosts());
   }, [dispatch]);
 
   return (
-    <div>
-        <Header/>
-        <Home/>
-    </div>
+
+    <BrowserRouter>
+
+   
+      <Header/>
+       
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>} exact/>
+        <Route path='/addUser' element={<AddUser/>} exact/>
+      
+
+      </Routes>
+
+
+
+
+    </BrowserRouter>
+
   )
 }
 
