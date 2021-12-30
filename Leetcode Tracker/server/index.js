5fccf4693805c34fa2c0cf9eb2de657ff4a91314
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-12-31 01:32:32 +0530

    added index.js for the server

---
+++ Leetcode Tracker/server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://pointless:dvz0tonoNLEgmBkj@cluster0.splay.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Server running on port : ${PORT}`)})
+
+//listening on the specified port by the server
+
+app.listen(PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+})
