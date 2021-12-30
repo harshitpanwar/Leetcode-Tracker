6f9745040d153b7bcf2f5e287d0ce1416cca2642
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-12-31 04:51:49 +0530

    added form and usedispatch funtionality

--- Leetcode Tracker/client/src/components/AddUser/AddUser.js
+++ Leetcode Tracker/client/src/components/AddUser/AddUser.js
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createPost } from '../../actions/posts';
+import './AddUser.css'
 
-function AddUser() {
+const AddUser = () => {
+
+    const [postData, setPostData] = useState({
+        name : '', username : '' , gender : ''
+    });
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+
+        try{
+            e.preventDefault();
+            dispatch(createPost(postData));
+            console.log(postData);
+        }
+        catch(error){
+            console.log(error)
+        }
+
+    }
+
     return (
-        <div>
-            add users here
-        </div>
+        <div className='outer'>
+        <form onSubmit={handleSubmit} autoComplete='off'>
+
+        <label>Name</label>
+
+            <input type="text" id="name" name="name" placeholder="Name.." value={postData.name} onChange={(e) => setPostData({...postData, name : e.target.value })}/>
+
+            <br/>
+
+            <label>Leetcode UserName</label>
+            <input type="text" id="username" name="username" placeholder="Leetcode username.." placeholder="Name.." value={postData.username} onChange={(e) => setPostData({...postData, username : e.target.value })}/>
+
+            <br/>
+
+            <label>Gender</label>
+                <select id="gender" name="gender" placeholder="Name.." value={postData.gender} onChange={(e) => setPostData({...postData, gender : e.target.value })}>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="None">Rather Not Say</option>
+                </select>
+
+            <input type="submit" value="submit"/>
+
+        </form>
+
+    </div>
     )
 }
 
